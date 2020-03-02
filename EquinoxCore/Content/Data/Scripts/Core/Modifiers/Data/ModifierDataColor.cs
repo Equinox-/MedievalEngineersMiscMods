12cf2adea81b2b02907900e3886bd5bb432af79e
Westin Miller <equinoxscripts@gmail.com>
2020-03-01 18:47:35 -0800

    Fix color drift across reloads

--- EquinoxCore/Content/Data/Scripts/Core/Modifiers/Data/ModifierDataColor.cs
+++ EquinoxCore/Content/Data/Scripts/Core/Modifiers/Data/ModifierDataColor.cs
@@ -8,8 +8,10 @@ namespace Equinox76561198048419394.Core.Modifiers.Data
 {
     public class ModifierDataColor : IModifierData
     {
+        // Legacy format: hex encoded HHSSVV len=6
+        // New format: "HHH+SSS+VVV" len=11
         private static readonly LruCache<string, ModifierDataColor> DataCache = new LruCache<string, ModifierDataColor>(16384);
-        
+
         private static readonly byte[] _charToBitsLut;
         private static readonly char[] _bitsToCharLut;
 
@@ -25,6 +27,20 @@ namespace Equinox76561198048419394.Core.Modifiers.Data
             }
         }
 
+        private static int CharToDigit(char c) => _charToBitsLut[Math.Min(0xFF, (int) c)];
+
+        private static int Char3ToInt(string s, int offset)
+        {
+            return CharToDigit(s[offset]) * 100 + CharToDigit(s[offset + 1]) * 10 + CharToDigit(s[offset + 2]);
+        }
+
+        private static void IntToChar3(int src, char[] dest, int offset)
+        {
+            dest[offset] = _bitsToCharLut[(src / 100) % 10];
+            dest[offset + 1] = _bitsToCharLut[(src / 10) % 10];
+            dest[offset + 2] = _bitsToCharLut[(src / 1) % 10];
+        }
+
         public readonly ColorDefinitionHSV Color;
 
         public ModifierDataColor(ColorDefinitionHSV hsv)
@@ -34,33 +50,39 @@ namespace Equinox76561198048419394.Core.Modifiers.Data
 
         public static ModifierDataColor Deserialize(string data)
         {
-            if (data == null || data.Length != 6)
+            if (data == null || (data.Length != 6 && data.Length != 11))
                 return null;
             return DataCache.GetOrCreate(data, dc =>
             {
-                var h = (byte) ((_charToBitsLut[Math.Min(0xFF, (int) dc[0])] << 4) | _charToBitsLut[Math.Min(0xFF, (int) dc[1])]);
-                var s = (byte) ((_charToBitsLut[Math.Min(0xFF, (int) dc[2])] << 4) | _charToBitsLut[Math.Min(0xFF, (int) dc[3])]);
-                var v = (byte) ((_charToBitsLut[Math.Min(0xFF, (int) dc[4])] << 4) | _charToBitsLut[Math.Min(0xFF, (int) dc[5])]);
-                var color = default(ColorDefinitionHSV);
-                color.H = h * 360 / 0xFF;
-                color.S = (s * 200 / 0xFF) - 100;
-                color.V = (v * 200 / 0xFF) - 100;
-                return new ModifierDataColor(color);
+                if (data.Length == 6)
+                {
+                    var h = (byte) ((CharToDigit(dc[0]) << 4) | CharToDigit(dc[1]));
+                    var s = (byte) ((CharToDigit(dc[2]) << 4) | CharToDigit(dc[3]));
+                    var v = (byte) ((CharToDigit(dc[4]) << 4) | CharToDigit(dc[5]));
+                    var color = default(ColorDefinitionHSV);
+                    color.H = h * 360 / 0xFF;
+                    color.S = (s * 200 / 0xFF) - 100;
+                    color.V = (v * 200 / 0xFF) - 100;
+                    return new ModifierDataColor(color);
+                }
+                else
+                {
+                    var h = Char3ToInt(dc, 0);
+                    var s = (dc[3] == '-' ? -1 : 1) * Char3ToInt(dc, 4);
+                    var v = (dc[7] == '-' ? -1 : 1) * Char3ToInt(dc, 8);
+                    return new ModifierDataColor(new ColorDefinitionHSV {H = h, S = s, V = v});
+                }
             });
         }
 
         public string Serialize()
         {
-            var buffer = new char[6];
-            var h = (byte) ((Color.H % 360) * 0xFF / 360);
-            var s = (byte) ((Color.S + 100) * 0xFF / 200);
-            var v = (byte) ((Color.V + 100) * 0xFF / 200);
-            buffer[0] = _bitsToCharLut[(h >> 4) & 0xF];
-            buffer[1] = _bitsToCharLut[(h >> 0) & 0xF];
-            buffer[2] = _bitsToCharLut[(s >> 4) & 0xF];
-            buffer[3] = _bitsToCharLut[(s >> 0) & 0xF];
-            buffer[4] = _bitsToCharLut[(v >> 4) & 0xF];
-            buffer[5] = _bitsToCharLut[(v >> 0) & 0xF];
+            var buffer = new char[11];
+            IntToChar3(Color.H % 360, buffer, 0);
+            buffer[3] = Color.S < 0 ? '-' : '+';
+            IntToChar3(MathHelper.Clamp(Math.Abs(Color.S), 0, 100), buffer, 4);
+            buffer[7] = Color.V < 0 ? '-' : '+';
+            IntToChar3(MathHelper.Clamp(Math.Abs(Color.V), 0, 100), buffer, 8);
             return new string(buffer);
         }
     }
--- EquinoxCoreTests/TestModifierDataSerialization.cs
+++ EquinoxCoreTests/TestModifierDataSerialization.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Diagnostics;
 using Equinox76561198048419394.Core.Modifiers.Data;
 using NUnit.Framework;
+using ObjectBuilders.Definitions.GUI;
+using VRage.Library.Utils;
 using VRageMath;
 
 namespace EquinoxCoreTests
@@ -11,9 +14,15 @@ namespace EquinoxCoreTests
         [Test]
         public void TestDataColor()
         {
-            foreach (var color in Color.AllNamedColors)
+            for (var seed = 0; seed< 10_000; seed++)
             {
-                var data = new ModifierDataColor(color.ColorToHSV());
+                var rand = new Random(seed);
+                var data = new ModifierDataColor(new ColorDefinitionHSV
+                {
+                    H = rand.Next(360),
+                    S = rand.Next(-100, 101),
+                    V = rand.Next(-100, 101)
+                });
                 var compared = ModifierDataColor.Deserialize(data.Serialize());
                 Assert.AreEqual(data.Color, compared.Color);
             }
